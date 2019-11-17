62ac39a755233cacdacc77c210dd7a34a5987e46
tlapay <trevor.n.lapay@gmail.com>
2019-11-17 08:18:16 -0700

    .

--- pyspark_gb.py
+++ pyspark_gb.py
@@ -8,12 +8,18 @@ data = spark.read.format("csv").option("header", "true").load("data/gdelt_encode
 stages = []
 # Index labels, adding metadata to the label column.
 # Fit on whole dataset to include all labels in index.
-labelIndexer = StringIndexer(inputCol="CAMEOCode", outputCol="indexedLabel").fit(data)
-stages += [labelIndexer]
+
 # Automatically identify categorical features, and index them.
 # Set maxCategories so features with > 4 distinct values are treated as continuous.
 numericCols = ['Source','Target','NumEvents','NumArts','SourceGeoType',
       'TargetGeoType', 'ActionGeoType','Month']
 assemblerInputs = numericCols
-data.show()
+data.printSchema()
+assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
+stages += [assembler]
+pipeline = Pipeline(stages = stages)
+pipelineModel = pipeline.fit(data)
+df = pipelineModel.transform(data)
+selectedCols = ['label', 'features'] + data.columns
+df = df.select(selectedCols)
 
