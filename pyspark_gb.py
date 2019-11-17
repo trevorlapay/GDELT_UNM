3e561ac592dec2c280951bee985a9d35c96383d7
Trevor La Pay <trevor.n.lapay@gmail.com>
2019-11-16 22:07:51 -0700

    ,

--- pyspark_gb.py
+++ pyspark_gb.py
@@ -1,6 +1,6 @@
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import GBTClassifier
-from pyspark.ml.feature import StringIndexer, VectorIndexer
+from pyspark.ml.feature import StringIndexer, VectorIndexer, VectorAssembler
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 # Load and parse the data file, converting it to a DataFrame.
@@ -11,8 +11,12 @@ data = spark.read.format("csv").option("header", "true").load("data/gdelt_encode
 labelIndexer = StringIndexer(inputCol="CAMEOCode", outputCol="indexedLabel").fit(data)
 # Automatically identify categorical features, and index them.
 # Set maxCategories so features with > 4 distinct values are treated as continuous.
+ignore = ['CAMEOCode'']
+assembler = VectorAssembler(
+    inputCols=[x for x in data.columns if x not in ignore],
+    outputCol='features')
 featureIndexer =\
-    VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
+    VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(assembler)
 
 # Split the data into training and test sets (30% held out for testing)
 (trainingData, testData) = data.randomSplit([0.7, 0.3])
